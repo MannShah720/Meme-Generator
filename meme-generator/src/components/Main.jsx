e5cf934dc82161f31ad5c87e441237854ed2af6e
MannShah720 <shahmann0108@gmail.com>
2025-07-12 21:31:55 +0100

    function to dynamically change top & bottom text

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react"
+
 export default  function Main() {
+
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    // Using controlled components so that...
+    // i can use one function for changing both top & bottom texts
+    function changeText(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className="form">
@@ -7,6 +26,8 @@ export default  function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={changeText}
+                        value={meme.topText}
                     />
                 </label>
 
@@ -15,14 +36,16 @@ export default  function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={changeText}
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
                 <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
